454bbbd5159b45250f83d184bc107635710177ba
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2023-01-11 10:15:13 -0600

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  products:any=[
+    {
+      name:'Producto Uno',
+      price: 123,
+      image: '../assets/image/product.jpg',
+      category: 'toys'
+    },
+    {
+      name:'Producto dos',
+      price: 485,
+      image: '../assets/image/product.jpg'
+    },
+    {
+      name:'Producto tres',
+      price: 741,
+      image: '../assets/image/product.jpg',
+      category: 'tools'
+    },
+    {
+      name:'Producto cuatro',
+      price: 956,
+      image: '../assets/image/product.jpg'
+    },
+    {
+      name:'Producto cinco',
+      price: 365,
+      image: '../assets/image/product.jpg'
+    },
+  ]
   title = 'My-try';
+  name= 'markos';
+  age = 35;
+  img = 'https://www.shutterstock.com/image-photo/cute-beagle-serious-face-lying-600w-1975831814.jpg';
+  btnDisabled = true;
+  emojis: string[] = ['😊','❤️','👍','🎶','😍'];
+  newEmoji = '';
+  toggleBtn(){
+    this.btnDisabled= !this.btnDisabled
+  }
+
+  person = {
+    name:'markos',
+    age: 35,
+  }
+
+  add(){
+    this.age +=1;
+  }
+  less(){
+    this.age -=1;
+  }
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+  addEmoji(){
+    this.emojis.push(this.newEmoji);
+    this.newEmoji='';
+  }
+  delete(i: number){
+    this.emojis.splice(i,1);
+    
+  }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,6 @@
+export interface Products{
+    name: string;
+    price: number;
+    image: string;
+    category?: string;// cuando se coloca el signo de interrogación es para indicar que el atributo es opcional.
+} 
