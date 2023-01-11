c6bd3b6992648b938f0d2fe6b121eab109222e55
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2023-01-11 11:25:32 -0600

    cambio de estilos en line a imagen desde una variable manejada con ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  widthImage= 25;
   products:any=[
     {
       name:'Producto Uno',
